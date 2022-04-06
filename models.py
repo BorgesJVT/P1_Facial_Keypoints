43f69ea863617d4c5b07e82c8d78fc8d9054660b
João Victor Torres Borges <joao.victor.tb@gmail.com>
2022-04-05 21:23:22 -0300

    initial net architecture

--- models.py
+++ models.py
@@ -19,19 +19,47 @@ class Net(nn.Module):
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 3x3 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 32, 3)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.drop1 = nn.Dropout(p=0.1)
+        # (32, 113, 113)
 
+
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.drop2 = nn.Dropout(p=0.1)
+        # (64, 55, 55)
+
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.drop3 = nn.Dropout(p=0.1)
+        # (128, 26, 26)
+
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.pool4 = nn.MaxPool2d(2, 2)
+        self.drop4 = nn.Dropout(p=0.1)
+        # (256, , )
+
+        self.linear1 = nn.Linear(128*26*26, 1000)
+        self.linear2 = nn.Linear(1000, 136)
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool1(F.relu(self.conv1(x)))
+        # x = x.view(x.size(0), -1)
+        # x = self.drop1(x)
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
         
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.linear1(x))
         
+        x = self.drop1(x)
+        x = self.linear2(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
